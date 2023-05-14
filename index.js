8a7ac93f9c4d424804d543699602223db6f873f4
devZahid <officialzahidhasan19932021@gmail.com>
2023-05-14 17:37:04 +0600

    adding id based route for the database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -22,28 +22,48 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const database = client.db("autoMedics");
+    const serviceCollections = database.collection("services");
+
+    app.get('/services', async (req,res) => {
+        const cursor = serviceCollections.find();
+        const result = await cursor.toArray();
+
+        res.send(result);
+
+    });
+    app.get('/services/:id', async (req,res) => {
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)}
+        const service = await serviceCollections.findOne(query);
+
+        res.send(service);
+    })
+    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-app.get('/',(req,res) => {
-    res.send('Welcome to automedics serversite');
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to automedics serversite");
+});
 
 app.listen(port, () => {
-    console.log('running on port :', port);
-})
+  console.log("running on port :", port);
+});
